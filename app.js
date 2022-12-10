188fd57871e9fecd65b502592ef5c2892401c27b
Vikranth Karu <vickyvikranth016@gmail.com>
2022-12-11 02:31:32 +0530

    2nd commit

--- app.js
+++ app.js
@@ -12,3 +12,55 @@ const app = new App({
     await app.start(process.env.PORT || port);
     console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
   })();
+  app.event('app_home_opened', async ({ event, client, context }) => {
+    try {
+      /* view.publish is the method that your app uses to push a view to the Home tab */
+      const result = await client.views.publish({
+  
+        /* the user that opened your app's app home */
+        user_id: event.user,
+  
+        /* the view object that appears in the app home*/
+        view: {
+          type: 'home',
+          callback_id: 'home_view',
+  
+          /* body of the view */
+          blocks: [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome to your _App's Home_* :tada:"
+              }
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+              }
+            },
+            {
+              "type": "actions",
+              "elements": [
+                {
+                  "type": "button",
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Click me!"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  });
