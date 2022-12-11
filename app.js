180e618c0b84e56195ac345bafddf2c91c35f69d
Vikranth Karu <vickyvikranth016@gmail.com>
2022-12-11 18:14:07 +0530

    commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode:false, // enable the following to use socket mode
+    socketMode:true, // enable the following to use socket mode
     appToken: process.env.APP_TOKEN
   });
   
@@ -18,8 +18,9 @@ const app = new App({
         });
         const userInfo = await conn.login(
             process.env.SF_USERNAME,
-    await app.start(process.env.PORT || port);
-    console.log(`⚡️ Slack Bolt app is running on port ${port}! ${userInfo.id}`);
+            process.env.SF_PASSWORD+process.env.SF_TOKEN);
+          await app.start(process.env.PORT || port);
+          console.log(`⚡️ Slack Bolt app is running on port ${port}! ${userInfo.id}`);
   })();
   app.event('app_home_opened', async ({ event, client, context }) => {
     try {
